ef3ba068ce49cc40dc75687eb8f040787e025227
hmtmcse <hmtm.cse@gmail.com>
2022-07-29 00:03:51 +0600

    raw ts project

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import {ProtectSite} from "./protect-site";
+console.log("---")
+
+new ProtectSite().init()
---
+++ src/protect-site.ts
@@ -0,0 +1,7 @@
+export class ProtectSite {
+
+    init() {
+        console.log("Init")
+    }
+
+}
