e322c62b879f84f3985f93f32fda46e09529c178
hmtmcse <hmtm.cse@gmail.com>
2022-07-29 07:58:01 +0600

    added

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,33 +1,32 @@
 export default {
-  mount: {
-    public: { url: '/', static: true },
-    src: { url: '/dist' },
-  },
-  plugins: [
-    [
-      '@snowpack/plugin-typescript',
-      {
-        /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
-        ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
-      },
+    mount: {
+        public: {url: '/', static: true},
+        src: {url: '/dist'},
+    },
+    plugins: [
+        [
+            '@snowpack/plugin-typescript',
+            {
+                /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
+                ...(process.versions.pnp ? {tsc: 'yarn pnpify tsc'} : {}),
+            },
+        ],
     ],
-  ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
-  optimize: {
-    "bundle": true,
-    minify: true,
-    target: 'es6',
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
+    routes: [
+        /* Enable an SPA Fallback in development: */
+        // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    ],
+    optimize: {
+        bundle: true,
+        minify: true,
+        target: 'es6',
+        sourcemap: false
+    },
+    packageOptions: {
+        /* ... */
+    },
+    devOptions: {
+        /* ... */
+    },
+    buildOptions: {},
 };
